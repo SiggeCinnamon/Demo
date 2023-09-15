0aa3d9d273fc126216717fa64044785001d0af00
SiggeCinnamon <Mattias.andersen@gmail.com>
2023-09-15 18:41:46 +0200

    ops.

--- src/main/java/com/example/demo/Config/Constants.java
+++ src/main/java/com/example/demo/Config/Constants.java
@@ -1,6 +1,6 @@
 package com.example.demo.Config;
 
 public interface Constants {
-    String URL_LINEDATA = "https://api.sl.se/api2/linedata.json?key=5da196d47f8f4e5facdb68d2e25b9eae&model=JourneyPatternPointOnLine&DefaultTransportModeCode=BUS";
-    String URL_SITEDATA = "https://api.sl.se/api2/linedata.json?key=5da196d47f8f4e5facdb68d2e25b9eae&model=site&DefaultTransportModeCode=BUS";
+    String URL_LINEDATA = "https://api.sl.se/api2/linedata.json?key=8e83dcb267ac4b03b109310a325d0dd2&model=JourneyPatternPointOnLine&DefaultTransportModeCode=BUS";
+    String URL_SITEDATA = "https://api.sl.se/api2/linedata.json?key=8e83dcb267ac4b03b109310a325d0dd2&model=site&DefaultTransportModeCode=BUS";
 }
